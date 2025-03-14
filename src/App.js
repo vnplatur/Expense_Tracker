6db765a6a09a1382bcb6eb7039889effa4ebe4ce
Vaijeenath Panchakshari <vnplatur@gmail.com>
2025-03-14 13:42:25 +0530

    connect to firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import { useState, useReducer } from "react";
+import { useState, useReducer, useEffect } from "react";
+import "./App.css";
+
+// components imports
 import ExpenseForm from "./components/ExpenseForm/ExpenseForm";
 import ExpenseInfo from "./components/ExpenseInfo/ExpenseInfo";
 import ExpenseList from "./components/ExpenseList/ExpenseList";
-import "./App.css";
+
+// react toasts
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// import firebase methods here
+import { doc, collection, addDoc, setDoc, getDocs,deleteDoc } from "firebase/firestore";
+import { db } from "./firebaseInit";
 
 const reducer = (state, action) => {
   const { payload } = action;
   switch (action.type) {
+    case "GET_EXPENSES": {
+      return {
+        expenses: payload.expenses
+      };
+    }
     case "ADD_EXPENSE": {
       return {
         expenses: [payload.expense, ...state.expenses]
@@ -17,58 +32,95 @@ const reducer = (state, action) => {
         expenses: state.expenses.filter((expense) => expense.id !== payload.id)
       };
     }
-    case "UPDATE":{
-      
+    case "UPDATE_EXPENSE": {
+      const expensesDuplicate = state.expenses;
+      expensesDuplicate[payload.expensePos] = payload.expense;
       return {
-        expenses: state.expenses.map((expense) =>
-          expense.id === payload.expense.id ? payload.expense : expense
-        ),
-      }
+        expenses: expensesDuplicate
+      };
     }
-    //add logic for updating the expense here
-    
     default:
       return state;
   }
 };
-// Use proper state management for populating the form in the expenseForm component on clicking the edit icon in the Transaction component
+
 function App() {
   const [state, dispatch] = useReducer(reducer, { expenses: [] });
-  const [editingExpense, setEditingExpense] = useState(null); 
+  const [expenseToUpdate, setExpenseToUpdate] = useState(null);
 
-  const addExpense = (expense) => {
-    dispatch({ type: "ADD_EXPENSE", payload: { expense } });
+  const getData = async () => {
+    const snapshot = await getDocs(collection(db, "expenses"));
+    const expenses = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data()
+    }));
+
+    dispatch({ type: "GET_EXPENSES", payload: { expenses } });
+    toast.success("Expenses retrived successfully.");
   };
 
-  const deleteExpense = (id) => {
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const addExpense = async (expense) => {
+    const expenseRef = collection(db, "expenses");
+    const docRef = await addDoc(expenseRef, expense);
+
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: { expense: { id: docRef.id, ...expense } }
+    });
+    toast.success("Expense added successfully.");
+  };
+
+  const deleteExpense = async (id) => {
+    // delete expense from firestore here
+    await deleteDoc(doc(db,"expenses",id));
+
     dispatch({ type: "REMOVE_EXPENSE", payload: { id } });
+    toast.success("Expense deleted successfully.");
   };
-  // Add dispatch function for updation
-  const update= (expense)=>{
-    dispatch({type:"UPDATE", payload:{expense}})
-    setEditingExpense(null);
-  }
 
-  const editExpense=(expense)=>{
-    setEditingExpense(expense);
-  }
+  const resetExpenseToUpdate = () => {
+    setExpenseToUpdate(null);
+  };
+
+  const updateExpense = async (expense) => {
+    const expensePos = state.expenses
+      .map(function (exp) {
+        return exp.id;
+      })
+      .indexOf(expense.id);
+
+    if (expensePos === -1) {
+      return false;
+    }
+
+    const expenseRef = doc(db, "expenses", expense.id);
+    await setDoc(expenseRef, expense);
+
+    dispatch({ type: "UPDATE_EXPENSE", payload: { expensePos, expense } });
+    toast.success("Expense updated successfully.");
+  };
+
   return (
     <>
+      <ToastContainer />
       <h2 className="mainHeading">Expense Tracker</h2>
       <div className="App">
-        <ExpenseForm 
-        addExpense={addExpense} 
-        editingExpense={editingExpense}
-        update={update}
-        // Pass the props for populating the form with expense text and amount
+        <ExpenseForm
+          addExpense={addExpense}
+          expenseToUpdate={expenseToUpdate}
+          updateExpense={updateExpense}
+          resetExpenseToUpdate={resetExpenseToUpdate}
         />
         <div className="expenseContainer">
           <ExpenseInfo expenses={state.expenses} />
           <ExpenseList
             expenses={state.expenses}
             deleteExpense={deleteExpense}
-            editExpense={editExpense}
-            // Pass props to update a transacation
+            changeExpenseToUpdate={setExpenseToUpdate}
           />
         </div>
       </div>
